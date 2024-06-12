0888c165d8bbff9aa3f5886228bb3c9555499d93
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-06-12 20:14:02 +0200

    update 2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'My first Nest deployment is succesfull! :)';
+    return 'My first Nest deployment is succesfull second time! :)';
   }
 }
